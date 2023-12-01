3ac83ef0daf5cdceccb33a6c6d180e770785c042
Tedo <tedojanelidze7@gmail.com>
2023-12-01 14:47:16 +0400

    added ready,error and loading status also StartScreen component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from 'react';
 import Header from './Header';
+import Loader from './Loader';
+import Error from './Error';
 import Main from './Main';
+import StartScreen from './StartScreen';
 
 const initialState = {
   questions: [],
@@ -28,7 +31,8 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch('http://localhost:9000/questions')
@@ -42,8 +46,9 @@ function App() {
       <Header />
 
       <Main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
